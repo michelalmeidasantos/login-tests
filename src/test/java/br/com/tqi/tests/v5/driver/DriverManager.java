05334bdaa2d1f48814a48dc3769ffc415556fc53
michel almeida santos <tqi_malmeida@uolinc.com>
2022-08-25 23:39:32 -0300

    Remoção de generic na v5

--- src/test/java/br/com/tqi/tests/v5/driver/DriverManager.java
+++ src/test/java/br/com/tqi/tests/v5/driver/DriverManager.java
@@ -1,14 +1,10 @@
 package br.com.tqi.tests.v5.driver;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.openqa.selenium.WebElement;
-
-import java.time.Duration;
 
 public class DriverManager {
     public DriverManager() {
@@ -26,20 +22,20 @@ public class DriverManager {
                     .build();
             service.start();
 
-        UiAutomator2Options options = new UiAutomator2Options() // UiAutomator2Options não existe em appium client < 8
-                .setDeviceName("emulator-5554")
-                .setPlatformName("Android")
-//        //forma 1 de abrir apk
-//                .setAppPackage("com.tqi.login")
-//                .setAppActivity("com.tqi.login.activity.LoginActivity")
-//        //forma 2 de abrir apk
-                .setApp("src/test/resources/app/login.apk")
-                .setFullReset(true)
-                .setNoReset(false)
-                .eventTimings();
-//        driver = new AndroidDriver(new URL("http://0.0.0.0:4723"), desiredCapabilities); // se appium client >= 8 não tem /wd/hub
-        driver = new AndroidDriver(service.getUrl(), options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        UiAutomator2Options options = new UiAutomator2Options() // UiAutomator2Options não existe em appium client < 8
+//                .setDeviceName("emulator-5554")
+//                .setPlatformName("Android")
+////        //forma 1 de abrir apk
+////                .setAppPackage("com.tqi.login")
+////                .setAppActivity("com.tqi.login.activity.LoginActivity")
+////        //forma 2 de abrir apk
+//                .setApp("src/test/resources/app/login.apk")
+//                .setFullReset(true)
+//                .setNoReset(false)
+//                .eventTimings();
+////        driver = new AndroidDriver(new URL("http://0.0.0.0:4723"), desiredCapabilities); // se appium client >= 8 não tem /wd/hub
+//        driver = new AndroidDriver(service.getUrl(), options);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             return driver;
         } else
             return driver;
